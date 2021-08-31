335aae067ae2560f0ae1810388666069c99353be
ZeldaDev <zelda_dev@outlook.com>
2021-08-31 16:13:39 +0200

    Added events

---
+++ Events/EventTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace NetDiscordRpc.Events
+{
+    [System.Flags]
+    public enum EventTypes
+    {
+        None = 0,
+        Spectate = 0x1,
+        Join = 0x2,
+        JoinRequest = 0x4
+    }
+}
---
+++ Events/Events.cs
@@ -0,0 +1,29 @@
+﻿using NetDiscordRpc.Message;
+using NetDiscordRpc.Message.Messages;
+
+namespace NetDiscordRpc.Events
+{
+    public delegate void OnReadyEvent(object sender, ReadyMessage args);
+    
+    public delegate void OnCloseEvent(object sender, CloseMessage args);
+    
+    public delegate void OnErrorEvent(object sender, ErrorMessage args);
+
+    public delegate void OnPresenceUpdateEvent(object sender, PresenceMessage args);
+    
+    public delegate void OnSubscribeEvent(object sender, SubscribeMessage args);
+    
+    public delegate void OnUnsubscribeEvent(object sender, UnsubscribeMessage args);
+    
+    public delegate void OnJoinEvent(object sender, JoinMessage args);
+    
+    public delegate void OnSpectateEvent(object sender, SpectateMessage args);
+    
+    public delegate void OnJoinRequestedEvent(object sender, JoinRequestMessage args);
+    
+    public delegate void OnConnectionEstablishedEvent(object sender, ConnectionEstablishedMessage args);
+    
+    public delegate void OnConnectionFailedEvent(object sender, ConnectionFailedMessage args);
+    
+    public delegate void OnRpcMessageEvent(object sender, IMessage msg);
+}
