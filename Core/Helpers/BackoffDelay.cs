12cad26dab688ff6131aa79afeeb3c44eecd0764
ZeldaDev <zelda_dev@outlook.com>
2021-08-31 16:12:33 +0200

    Added helpers

---
+++ Core/Helpers/BackoffDelay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace NetDiscordRpc.Core.Helpers
+{
+    internal class BackoffDelay
+    {
+        public int Maximum { get; private set; }
+        
+        public int Minimum { get; private set; }
+        
+        public int Current { get { return _current; } }
+        private int _current;
+        
+        public int Fails { get { return _fails; } }
+        private int _fails;
+        
+        public Random Random { get; set; }
+
+        private BackoffDelay() { }
+        public BackoffDelay(int min, int max): this(min, max, new Random()) { }
+        public BackoffDelay(int min, int max, Random random)
+        {
+            Minimum = min;
+            Maximum = max;
+
+            _current = min;
+            _fails = 0;
+            Random = random;
+        }
+        
+        public void Reset()
+        {
+            _fails = 0;
+            _current = Minimum;
+        }
+
+        public int NextDelay()
+        {
+            _fails++;
+
+            var diff = (double)(Maximum - Minimum) / 100f;
+            _current = (int)Math.Floor(diff * _fails) + Minimum;
+            
+            return Math.Min(Math.Max(_current, Minimum), Maximum);
+        }
+    }
+}
---
+++ Core/Helpers/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace NetDiscordRpc.Core.Helpers
+{
+    public static class StringHelper
+    {
+        public static string GetNullOrString(this string str) => str.Length == 0 || string.IsNullOrEmpty(str.Trim()) ? null : str;
+        
+        public static bool WithinLength(this string str, int bytes) => str.WithinLength(bytes, Encoding.UTF8);
+        
+        public static bool WithinLength(this string str, int bytes, Encoding encoding) => encoding.GetByteCount(str) <= bytes;
+
+        public static string ToCamelCase(this string str)
+        {
+            return str?.ToLower()
+                .Split(new[] { "_", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => char.ToUpper(s[0]) + s.Substring(1, s.Length - 1))
+                .Aggregate(string.Empty, (s1, s2) => s1 + s2);
+        }
+        
+        public static string ToSnakeCase(this string str)
+        {
+            if (str == null) return null;
+            
+            var concat = string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString()).ToArray());
+            
+            return concat.ToUpper();
+        }
+    }
+}
